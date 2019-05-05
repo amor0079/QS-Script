96697b028f6e10c66795d47ac01671b08157eb78
Amor <394508359@qq.com>
2019-05-06 07:06:12 +0800

    modify list[a] position structure

--- TK-gui.py
+++ TK-gui.py
@@ -1,7 +1,7 @@
 '''
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2019/4/2 6:59
+# @Time    : 2019/5/6 6:59
 # @Author  : Amor
 # @Site    : 
 # @File    : TK-gui.py
@@ -16,7 +16,7 @@ root = Tk()
 group = LabelFrame(root, text = '请选择要输出的条件票', padx = 30, pady = 30)
 group.pack(padx = 10, pady = 10)
 
-
+# 定义checkbutton,一个元祖
 LANGS = (
         'ELB包装条件票',
         'ELCK贴片加工核准单',
@@ -35,65 +35,61 @@ LANGS = (
         'ELREF塑壳材料核准单',
         'ELPCB线路板材料核准单',
         'ELPF生产流程表'
-        ) #定义一个元祖
-
+        )
 v = [] #定义一个列表
 for long in LANGS:
     intVar = IntVar()
     v.append(intVar)
     b = Checkbutton(group,
                         text = long,
-                        # onvalue = num,
-                        # offvalue = 0 ,
                         variable = intVar,
-                        # command = change
+                        # command = choice_selection
                         )
     b.pack(anchor = W)
 
 #将选择添加进一个列表
 a = []
-for var in v:
-    a.append(var.get())
-print(a)
-
 #定义主逻辑
 def show():
-    template0_ELB = r"C:\Users\win\Desktop\GUI_TJP_script"
-    # template1_ELCK =
-    #     # template2_ELF =
-    #     # template3_ELM =
-    #     # template4_ELN =
-    #     # template5_ELP =
-    #     # template6_ELQC =
-    #     # template7_ELW =
-    #     # template8_ELHC =
-    #     # template9_ELSMT =
-    #     # template10_ELT =
-    #     # template11_ELY =
-    #     # template12_FIL_H =
-    #     # template13_FIL_X =
-    #     # template14_REF =
-    #     # template15_PCB =
-    #     # template16_ELPF =
-
-    #主判断
-    if a[0] ==1:
-        document0 =MailMerge(template)
-        cust_1 = {'name': 1, #需修改读入
-                  'date1': '{:%B %d,%Y}'.format(date.today()),
-                  'date2': '{:%Y-%m-%d}'.format(date.today()),
-                  # 'spec_no': e2.get(),
-                  # 'engineer': e3.get(),
-                  # 'wide': e4.get(),
-                  # 'length': e5.get(),
-                  # 'n_colour': sm_colour,
-                  # 'up_sm_colour': up_sm_colour,
-                  # 'jixing': jixing,
-                  }
-        document0.merge_pages([cust_1])
-        # print(document.get_merge_fields())
-        document.write("./{}.docx".format(12)) #需修改
-        # e1.delete(0, END)
+    for var in v:
+        a.append(var.get())
+    print(a)
+    # template0_ELB = r"C:\Users\win\Desktop\GUI_TJP_script"
+    # # template1_ELCK =
+    # #     # template2_ELF =
+    # #     # template3_ELM =
+    # #     # template4_ELN =
+    # #     # template5_ELP =
+    # #     # template6_ELQC =
+    # #     # template7_ELW =
+    # #     # template8_ELHC =
+    # #     # template9_ELSMT =
+    # #     # template10_ELT =
+    # #     # template11_ELY =
+    # #     # template12_FIL_H =
+    # #     # template13_FIL_X =
+    # #     # template14_REF =
+    # #     # template15_PCB =
+    # #     # template16_ELPF =
+    #
+    # #主判断
+    # if a[0] ==1:
+    #     document0 =MailMerge(template)
+    #     cust_1 = {'name': 1, #需修改读入
+    #               'date1': '{:%B %d,%Y}'.format(date.today()),
+    #               'date2': '{:%Y-%m-%d}'.format(date.today()),
+    #               # 'spec_no': e2.get(),
+    #               # 'engineer': e3.get(),
+    #               # 'wide': e4.get(),
+    #               # 'length': e5.get(),
+    #               # 'n_colour': sm_colour,
+    #               # 'up_sm_colour': up_sm_colour,
+    #               # 'jixing': jixing,
+    #               }
+    #     document0.merge_pages([cust_1])
+    #     # print(document.get_merge_fields())
+    #     document.write("./{}.docx".format(12)) #需修改
+    #     # e1.delete(0, END)
 
 
 
@@ -103,18 +99,4 @@ Button(root, text = "确认输出文档", width = 10, command = show)\
 Button(root, text = "退出", width = 10, command = root.quit)\
              .pack(side='right', padx = 10, pady = 5)
 
-
-
-# def change(self):
-#         # 将self.chars列表转换成元素为str的列表
-#     new_li = [str(v.get()) for v in LANGS]
-#         # 将new_li列表连接成字符串
-#     st = ', '.join(new_li)
-#     messagebox.showinfo(title=None, message=st)
-print(a)
 mainloop()
-
-# a = []
-# for var in v:
-#     a.append(var.get())
-# print(a)
