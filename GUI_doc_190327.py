9bd3d1e22c95156d0923422da5da8927eecb494e
Amor <394508359@qq.com>
2019-06-03 23:12:46 +0800

    fix some bug

--- GUI_doc_190327.py
+++ GUI_doc_190327.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/3/26 15:31
 # @Author  : Amor
-# @Site    : 
+# @Site    :
 # @File    : GUI_doc.py
 # @Software: PyCharm
 from tkinter import *
@@ -27,14 +27,16 @@ chip_Inf = {
     'A': ('A', 'Amber', 'AlGalnP/GaAs', 2.0, 2.5, 607, 20),
     'V': ('V', 'Super Bright Green', 'InGaN/GaN', 3.5, 4.0, 525, 35),
     'X': ('V', 'Super Bright Green', 'InGaN/GaN', 3.5, 4.0, 535, 35),
-    'C': ('C', 'Super Bright White', 'InGaN/GaN', 3.2, 3.8)}
+    'C': ('C', 'Super Bright White', 'InGaN/GaN', 3.2, 3.8, '/' , '/')}
 
 
 #筛选晶片函数
 def name_resolution():
-    global chip, chip_len, polarity, tem_choice
+    global chip, chip_len, polarity, tem_choice, ink_choice
     a1 = e1.get() #获取输入
     tem_choice = a1.split('-')[0][-1] #依据最后一位来选择模板
+
+    ink_choice = a1.split('-')[2][0] #依据第三段第一位来判断油墨颜色。
 
     colour_len_split = a1.split('-')[1] #使用split函数分隔输入信息
     chip = re.sub('[^a-zA-Z]', '', colour_len_split)
@@ -72,6 +74,13 @@ def jx():
 def message():
     messagebox.showinfo(title= 'Congratulation', message='规格书创建成功')
 
+def ink(): #判断油墨颜色
+    global ref_ink
+    if ink_choice == '2':
+        ref_ink = 'black'
+    elif ink_choice == '1':
+        ref_ink = 'gray'
+    return ref_ink
 
 #选择模板函数
 def choice_tem():
@@ -87,21 +96,30 @@ def choice_tem():
     #判断
     sin_mul() #判断晶片单复数。
     jx() #判断晶片极性
+    ink() #判断塑壳油墨
     up_sm_colour = sm_colour.upper() #颜色大写
+    up_colour1 = co1.upper()
 
     if tem_choice == 'S': #选择ZDS模板
         document = MailMerge(tem0_ZDS) #选择模板
         cust_1 = {'name': e1.get(),
                   'date1': '{:%B %d,%Y}'.format(date.today()),
                   'date2': '{:%Y-%m-%d}'.format(date.today()),
-                  'spec_no': e2.get(),
-                  'engineer': e3.get(),
-                  'wide': e4.get(),
-                  'length': e5.get(),
+                  'spec_no': str(e2.get()),
+                  'engineer': str(e3.get()),
+                  'wide': str(e4.get()),
+                  'length': str(e5.get()),
                   'n_colour': sm_colour,
                   'up_sm_colour': up_sm_colour,
                   'jixing': jixing,
-                  # 'colour_1':co1,
+                  'colour_1':co1,
+                  'element' : co1_ele,
+                  'ink': ref_ink,
+                  'vft': str(co1_VF_t),
+                  'vfm': str(co1_VF_m),
+                  'bo': str(co1_bo),
+                  'bbo': str(co1_bbo),
+                  'upcolour1': up_colour1
                   }
         document.merge_pages([cust_1])
         document.write("./{}.docx".format(e1.get()))
@@ -228,21 +246,26 @@ def choice_tem():
     #需要加入try except
 
 def colour(): #判断颜色个数赋值晶片参数
+    global co1, co2, co3, co4, co1_ele, co1_VF_m, co1_VF_t, co1_bo, co1_bbo
     if chip_len == 1:
-        colour1 = choice_chip_inf[0][0]
-        pass    #晶片其他参数
+        co1 = choice_chip_inf[0][1]
+        co1_ele = choice_chip_inf[0][2]
+        co1_VF_t = choice_chip_inf[0][3]
+        co1_VF_m = choice_chip_inf[0][4]
+        co1_bo = choice_chip_inf[0][5]
+        co1_bbo = choice_chip_inf[0][6]#晶片其他参数
     elif chip_len == 2:
-        colour1 = choice_chip_inf[0][0]
-        colour2 = choice_chip_inf[1][0]
+        co1 = choice_chip_inf[0][1]
+        co2 = choice_chip_inf[1][1]
     elif chip_len == 3:
-        colour1 = choice_chip_inf[0][0]
-        colour2 = choice_chip_inf[1][0]
-        colour3 = choice_chip_inf[2][0]
+        co1 = choice_chip_inf[0][1]
+        co2 = choice_chip_inf[1][1]
+        co3 = choice_chip_inf[2][1]
     elif chip_len == 4:
-        colour1 = choice_chip_inf[0][0]
-        colour2 = choice_chip_inf[1][0]
-        colour3 = choice_chip_inf[2][0]
-        colour4 = choice_chip_inf[3][0]
+        co1 = choice_chip_inf[0][1]
+        co2 = choice_chip_inf[1][1]
+        co3 = choice_chip_inf[2][1]
+        co4 = choice_chip_inf[3][1]
     #需要加入try except
 
 
